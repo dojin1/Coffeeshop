ec8cc9b9b232d35a6b4473826979c733187395ac
ana27 <ana27@daum.net>
2021-08-04 11:03:57 +0900

    0804

--- Coffeeshop/src/Coffeeshop.java
+++ Coffeeshop/src/Coffeeshop.java
@@ -7,7 +7,7 @@ public class Coffeeshop {
 		Menu menu=new Menu();
 		//menu.showMenu();
 		Scanner s=new Scanner(System.in);
-		Scanner sc=new Scanner(System.in);
+		Scanner s1=new Scanner(System.in);
 		System.out.println("<단축키를 입력해 주세요.> m:메뉴관리,o:주문받기,s:실적받기, 'x'또는'X' 입력시 프로그램 종료");
 		String n=s.nextLine();
 		while(!n.equals("x")) {
@@ -19,33 +19,15 @@ public class Coffeeshop {
 				n=s.nextLine();	
 				while(!n.equals("q")) { // "q"가 아닌 동안
 					if(n.equals("c") || n.equals("C")) { // "c":새메뉴추가 작업 출력
-						System.out.println("새로운 메뉴를 추가합니다. 새 메뉴명을 입력해 주세요");
-						n=s.nextLine();// 새메뉴명 읽기
-						menu.addName(n);// 메뉴명 추가 add name
-						System.out.println("가격을 입력해 주세요");
-						n=s.nextLine();	// 새가격 읽기
-						menu.addPrice(n);// 메뉴가격 추가 add price
-						System.out.println("<추가완료> 'r'키로 추가된 목록확인");
+						menu.appendMenu();
 					}else if(n.equals("r") || n.equals("R")) { // "r":목록보기 출력
 						menu.showMenu();// 메뉴목록보기.
 						System.out.println("메뉴작업을 선택하세요.(c:추가,r:목록보기,u:수정,d:삭제,q:메뉴관리 종료)");
 					}else if(n.equals("u") || n.equals("U")) { // "u":기존메뉴 수정(이름/가격) 수정출력
-						System.out.println("기존메뉴 수정(이름/가격) 수정할 메뉴번호를 입력해 주세요");
-						int a=sc.nextInt();// 수정할 메뉴번호 읽기
-						System.out.println("수정할 메뉴명을 입력해 주세요.");
-						n=s.nextLine();// 수정할 메뉴명 읽기
-						System.out.println("수정할 가격을 입력해 주세요.");
-						int c=sc.nextInt();// 수정할 가격 읽기
-						menu.changeMenu(a, n, c);// 메뉴번호에 해당하는 메뉴명&가격 수정
-						System.out.println("<수정완료> 메뉴목록을 출력합니다.");
-						menu.showMenu();
+						menu.changeMenu();
 						System.out.println("메뉴작업을 선택하세요.(c:추가,r:목록보기,u:수정,d:삭제,q:메뉴관리 종료)");
 					}else if(n.equals("d") || n.equals("D")) { // "d":기존메뉴 삭제 삭제출력
-						System.out.println("<기존메뉴 삭제> 삭제할 메뉴번호를 입력해 주세요.");
-						int t=sc.nextInt();// 삭제할 메뉴번호 읽기 
-						menu.deleteMenu(t);// 메뉴 삭제
-						System.out.println("<삭제완료> 메뉴목록을 출력합니다.");
-						menu.showMenu();
+						menu.deleteMenu();
 					}else if(n.equals("Q")) {
 						break;
 					}else {
--- Coffeeshop/src/Menu.java
+++ Coffeeshop/src/Menu.java
@@ -6,11 +6,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu { // 선언(정의) only. not 실행코드
 	private ArrayList<String> alName;
 	private ArrayList<Integer> alPrice;
-	
 
 	
 	  Menu() {
@@ -28,6 +28,7 @@ public class Menu { // 선언(정의) only. not 실행코드
 	  }
 	 
 	
+	  
 	/*
 	 * 생성자 - 일종의 메소드 용도: 주로 초기화 작업용. 명명법: 이름이 클래스와 같아야 함. 반환값: 반환값이 없다.(반환할 수 없이,
 	 * 작업만 실행) 실행/호출: new연산자가 실행된 직후, 자동실행 다른 메소드/생성자를 호출할 수 있다.
@@ -39,17 +40,42 @@ public class Menu { // 선언(정의) only. not 실행코드
 	void addPrice(int price) {
 		this.alPrice.add(price);
 	}
-
 	void addPrice(String price) {
 		this.alPrice.add(Integer.parseInt(price));
 	}
-	void changeMenu(int menu_num,String name,int price) {
-		this.alName.set(menu_num-1, name);
-		this.alPrice.set(menu_num-1, price);
+	void appendMenu() {
+		Scanner s=new Scanner(System.in);
+		System.out.println("새로운 메뉴를 추가합니다. 새 메뉴명을 입력해 주세요");
+		String n=s.nextLine();// 새메뉴명 읽기
+		addName(n);// 메뉴명 추가 add name
+		System.out.println("가격을 입력해 주세요");
+		n=s.nextLine();	// 새가격 읽기
+		addPrice(n);// 메뉴가격 추가 add price
+		System.out.println("<추가완료> 'r'키로 추가된 목록확인");
 	}
-	void deleteMenu(int menu_num) {
-		this.alName.remove(menu_num-1);
-		this.alPrice.remove(menu_num-1);
+
+	void changeMenu() {
+		Scanner s=new Scanner(System.in);
+		Scanner s1=new Scanner(System.in);
+		System.out.println("기존메뉴 수정(이름/가격) 수정할 메뉴번호를 입력해 주세요");
+		int n=s1.nextInt();// 수정할 메뉴번호 읽기
+		System.out.println("수정할 메뉴명을 입력해 주세요.");
+		String m=s.nextLine();// 수정할 메뉴명 읽기
+		System.out.println("수정할 가격을 입력해 주세요.");
+		int p=s1.nextInt();// 수정할 가격 읽기
+		this.alName.set(n-1, m);
+		this.alPrice.set(n-1, p);
+		System.out.println("<수정완료> 메뉴목록을 출력합니다.");
+		showMenu();
+	}
+	void deleteMenu() {
+		Scanner s1=new Scanner(System.in);
+		System.out.println("<기존메뉴 삭제> 삭제할 메뉴번호를 입력해 주세요.");
+		int t=s1.nextInt();// 삭제할 메뉴번호 읽기 
+		this.alName.remove(t-1);
+		this.alPrice.remove(t-1);
+		System.out.println("<삭제완료> 메뉴목록을 출력합니다.");
+		showMenu();
 	}
 	void showMenu() {
 		for (int i = 0; i < this.alName.size(); i++) {
